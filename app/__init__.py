12bc3af9b0443f7bd5bdc0d7f9a1a7191aeb4e6b
avihu <avihupinko@gmail.com>
2017-05-09 18:09:05 +0300

    merged back parties branch to master

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
+import flask_sqlalchemy
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('flask_config')
-db = SQLAlchemy(app)
+db = flask_sqlalchemy.SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -13,32 +13,10 @@ class AppTestCase(unittest.TestCase):
         app.data = {}
         app.free_id = 0
 
-    def test_cant_enter_without_login(self):
-        # Check if login link is visible
-        index_page = self.app.get('/index')
-        assert 'Log in' not in index_page
-
-        # def test_login(self):
-        #     # Check if login link is visible
-        #     index_page = self.app.get('/')
-        #     assert 'Log in' in index_page.data
-        #
-        #     # Check if form visible
-        #     login_page = self.app.get('/login')
-        #     assert 'login' in login_page.data
-        #     assert 'password' in login_page.data
-        #
-        #     # Check invalid data
-        #     invalid_login = self.app.post('login', data=dict(login='invalid', password='invalid'), follow_redirects=True)
-        #     assert 'Invalid credentials' in invalid_login.data
-        #
-        #     # Check valid credentials
-        #     valid_login = self.app.post('login', data=dict(login='admin', password='demo'), follow_redirects=True)
-        #     assert 'You were logged in' in valid_login.data
-        #
-        #     # Test logging out
-        #     logout = self.app.get('logout', follow_redirects=True)
-        #     assert 'You were logged out' in logout.data
+    def test_false_login(self):
+        invalid_login = self.app.post('login', data={'first_name': 'avihu', 'last_name': 'pinko'},
+                                      follow_redirects=True)
+        assert 'Invalid credentials' in invalid_login.data
 
 
 # Run tests if script was executed directly from the shell
