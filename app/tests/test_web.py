b057e30604d741c34fb39e1ed183350c18b3044b
avihu <avihupinko@gmail.com>
2017-05-10 12:54:58 +0300

    start working on integration testing

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -21,22 +21,30 @@ class AppTestCase(LiveServerTestCase):
         self.driver.quit()
 
     def test_server_is_up_and_running(self):
-        response = self.app.get(self.get_server_url(), content_type='application/json')
+        response = self.app.get(self.get_server_url())
         self.assertEqual(response.code, 200)
 
-    def test_voting(self):
-        self.browser.get(' http://127.0.0.1:5000/login')
-        firstname = self.browser.find_element_by_name('first_name')
+    def test_getting_to_voting_page(self):
+        firstname = self.driver.find_element_by_name('first_name')
         firstname.send_keys('yulia')
-        lastname = self.browser.find_element_by_name('last_name')
+        lastname = self.driver.find_element_by_name('last_name')
         lastname.send_keys('zorin')
-
-        id = self.browser.find_element_by_name('id')
+        id = self.driver.find_element_by_name('id')
         id.send_keys('678')
+        self.driver.find_element_by_name("submit").click()
+        # check if user where able to enter parties page
+        self.assertIn(u'ברוכים הבאים , yulia zorin', self.driver.find_element_by_tag_name('h1'))
 
-        form = self.browser.find_element_by_id('loginForm')
-        form.submit()
-
+    def test_user_not_in_database(self):
+        firstname = self.driver.find_element_by_name('first_name')
+        firstname.send_keys('yulia')
+        lastname = self.driver.find_element_by_name('last_name')
+        lastname.send_keys('zorin')
+        id = self.driver.find_element_by_name('id')
+        id.send_keys('987')
+        self.driver.find_element_by_name("submit").click()
+        # check if user where able to enter parties page
+        self.assertIn(u'המצביע אינו מופיע בבסיס הנתונים', self.driver.find_element_by_class_name("error"))
 
 # Run tests if script was executed directly from the shell
 if (__name__ == '__main__'):
