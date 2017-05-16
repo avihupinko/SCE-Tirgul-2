83419d32c10681272cac3df97f6049297b86ffb6
avihu <avihupinko@gmail.com>
2017-05-16 11:22:23 +0300

    fixed integration testing

---
+++ app/tests/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('flask_config')
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
