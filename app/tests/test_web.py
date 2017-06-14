419a04236048276a7031137454cfbf97f8293a98
avihu <avihupinko@gmail.com>
2017-06-14 09:26:49 +0300

    fixed test cases

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -64,12 +64,8 @@ class AppTestCase(LiveServerTestCase):
         id.send_keys(Keys.ENTER)
         assert u'לצורך הצבעה, בחר את המפלגה הרצויה' in self.browser.page_source
         # select party
-        # select_elements = self.browser.find_elements_by_name('partyId')
-        # select_elements[0].submit()
-        select = self.browser.find_element_by_xpath("(//input[@name='partyId'])[0]")
-        select.click()
-        select.submit()
-
+        select_elements = self.browser.find_elements_by_name('partyId')
+        select_elements[0].submit()
         assert u'האם ברצונך לאשר את הצבעתך' in self.browser.page_source
 
         # confirm selected party
