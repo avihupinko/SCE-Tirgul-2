f87481979f647ee10005240aac8b7d50cffb8c9c
avihu <avihupinko@gmail.com>
2017-06-18 14:29:58 +0300

    fixed test cases

--- app/tests/test_web.py
+++ app/tests/test_web.py
@@ -67,8 +67,8 @@ class AppTestCase(LiveServerTestCase):
         id.send_keys(Keys.ENTER)
         assert u'לצורך הצבעה, בחר את המפלגה הרצויה' in self.browser.page_source
         # select party
-        self.browser.find_element_by_tag_name('img').click()
-        self.browser.find_element_by_class_name('btn').submit()
+        self.browser.find_element_by_tag_name('img').submit()
+        self.browser.find_element_by_class_name('btn btn-default').submit()
 
         assert u'האם ברצונך לאשר את הצבעתך' in self.browser.page_source
 
