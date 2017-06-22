24dcd6fe7de3932c7a7ebbb374c8b831897881e4
JZorina <yuliazo@ac.sce.ac.il>
2017-06-22 10:36:43 +0300

    Update config1.sh
    update config1.sh

--- config1.sh
+++ config1.sh
@@ -1,27 +1,49 @@
-#!/bin/bash
-
-#update system
-sudo yum -y update && upgrade
-
-#install git python and devtools
-sudo yum -y install git-all centos-relese-SCL python-setuptools python-setuptools-devel python-devel
-sudo yum -y groupinstall "Development Tools"
-
-#install pip
+sudo yum install git-all -y
+sudo yum install wget -y
 sudo easy_install pip
+sudo pip install alembic==0.9.1
+sudo pip install aniso8601==1.2.0
+sudo pip install Babel==2.4.0
+sudo pip install blinker==1.4
+sudo pip install click==6.7
+sudo pip install CouchDB==1.1
+sudo pip install Flask==0.12
+sudo pip install Flask-Babel==0.11.1
 
-#clone proejct repo
-git clone https://github.com/avihupinko/SCE-Tirgul-2.git
+sudo pip install Flask-Blogging==0.7.4
+sudo pip install Flask-Cache==0.13.1
+sudo pip install Flask-CouchDB==0.2.1
+sudo pip install Flask-Login==0.4.0
+sudo pip install Flask-Mail==0.9.1
+sudo pip install Flask-Migrate==1.2.0
+sudo pip install Flask-OpenID==1.2.5
+sudo pip install Flask-Principal==0.4.0
+sudo pip install Flask-RESTful==0.3.5
+sudo pip install flask-restful-swagger==0.19
+sudo pip install Flask-Script==2.0.5
+sudo pip install Flask-SQLAlchemy==2.2
+sudo pip install Flask-Testing==0.4.2
+sudo pip install Flask-WhooshAlchemy==0.56
+sudo pip install Flask-WTF==0.10.2
+sudo pip install itsdangerous==0.24
+sudo pip install Jinja2==2.9.5
+sudo pip install Mako==1.0.6
+sudo pip install Markdown==2.6.8
+sudo pip install MarkupSafe==1.0
+sudo pip install cloudshell-automation-api
+sudo pip install python-dateutil==2.6.0
+sudo pip install python-editor==1.0.3
+sudo pip install python-openid==2.2.5
+sudo pip install python-slugify==1.2.1
+sudo pip install pytz==2016.10
+sudo pip install six==1.10.0
+sudo pip install SQLAlchemy==1.1.6
+sudo pip install Unidecode==0.4.20
+sudo pip install Werkzeug==0.12.1
+sudo pip install wheel==0.24.0
+sudo pip install Whoosh==2.7.4
+sudo pip install WTForms==2.1
+git clone https://github.com/avihupinko/SCE-Tirgul-2
 cd SCE-Tirgul-2
-
-#install our app requirements
-sudo pip install -r requirements.txt
-
-#create db
-python db_create.py
-
-#redirects all traffic from port 80 to 5000
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
-
-#run app
 sudo nohup python run.py &
+echo hi 
